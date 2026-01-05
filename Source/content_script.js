891df6022e2712c461461611da08fa46dff1b449
Fabio Neves <fneves@wrapbook.com>
2026-01-05 12:43:59 -0500

    This should be it, I think. Now to build it.

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,50 +1,42 @@
 walk(document.body);
 
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-	
-	var tagName = node.tagName ? node.tagName.toLowerCase() : "";
-	if (tagName == 'input' || tagName == 'textarea') {
-		return;
-	}
-	if (node.classList && node.classList.contains('ace_editor')) {
-		return;
-	}
+function walk(node) {
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+  let child, next;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+  let tagName = node.tagName ? node.tagName.toLowerCase() : "";
+  if (tagName == "input" || tagName == "textarea") {
+    return;
+  }
+  if (node.classList && node.classList.contains("ace_editor")) {
+    return;
+  }
+
+  switch (node.nodeType) {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
+function handleText(textNode) {
+  let v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+  v = v.replace(/\bSlop\b/g, "Nadella");
+  v = v.replace(/\bslop\b/g, "nadella");
+
+  textNode.nodeValue = v;
 }
-
-
